8c9a1f7411fd3aa1eff2e119fcc3c418cfd13887
Jay Ramos <89936073+ramosJay@users.noreply.github.com>
2021-09-20 20:16:38 -1000

    Finish assignment
    ziplist WOD

---
+++ ziplist.js
@@ -0,0 +1,15 @@
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+function zipList(l1, l2) {
+  const result = [];
+  for (let i = 0; i < l1.length; i++) {
+    result.push(l1[i], l2[i]);
+  }
+  return result;
+}
+console.log(zipList(list1, list2));
+
+function zipListTheSimpleWay(l1, l2) {
+  return _.flatten(_.zip(l1, l2));
+}
+console.log(zipListTheSimpleWay(list1, list2));
